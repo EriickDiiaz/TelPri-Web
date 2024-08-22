fd0d9439162a287fb7a6db9f21817268f3e83c6e
Erick Desktop <erick.diaz.1.2@gmail.com>
2024-08-22 19:49:29 -0400

    Se agregan DataTables, Iconos y estilos a sección Campos

--- resources/views/campos/edit.blade.php
+++ resources/views/campos/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layout/template')
+@extends('layout/template') 
 
 @section('title','Campos | Modificar Campo')
 @section('contenido')
@@ -6,6 +6,7 @@
 <!-- Mensajes y Notificaciones -->
 @if ($errors->any())
     <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <i class="fa-solid fa-triangle-exclamation"></i>
         <strong>¡Uy!</strong> Revisa los siguientes errores antes de continuar.
         <ul>
             @foreach($errors->all() as $error)
@@ -17,9 +18,8 @@
 @endif
 
 <!-- Titulo de la Sección -->
-<div class="d-flex">
-    <i class="bi bi-building-exclamation" style="font-size:150%;"></i>
-    <h2 class="align-middle">Modificar Campo.</h2>
+<div class="d-flex">    
+    <h2><i class="fa-solid fa-pen-to-square m-2"></i>Modificar Campo.</h2>
 </div>
 
 <!--Contenido de la Sección -->
@@ -27,32 +27,32 @@
     @method("PUT")
     @csrf
 
-    <div>
+    <div class="mb-2">
         <label for="nombre" class="col-sm-2 col-form-label">Nombre del campo:</label>
         <div class="col-sm-5">
             <input type="text" class="form-control" name="nombre" id="nombre" value="{{ $campo->nombre }}" required>
         </div>
     </div>
 
-    <div>
+    <div class="mb-2">
         <label for="descripcion" class="col-sm-2 col-form-label">Descripcion del campo:</label>
         <div class="col-sm-5">
             <input type="text" class="form-control" name="descripcion" id="descripcion" value="{{ $campo->descripcion }}" required>
         </div>
     </div>
 
-    <div class="mt-3">
+    <div class="mt-3 d-flex justify-content-between col-5">
         <a href="{{ url('campos') }}" class="btn btn-outline-danger btn-sm">
-            <i class="bi bi-backspace"></i>
-            <span>Regresar</span>
+            <span>
+                <i class="fa-solid fa-delete-left m-2"></i>Regresar
+            </span>
         </a>
-        |
         <button type="submit" class="btn btn-outline-primary btn-sm">
-            <i class="bi bi-building-exclamation"></i>
-            <span>Actualizar</span>
+            <span>
+                <i class="fa-solid fa-check m-2"></i>Actualizar Campo
+            </span>
         </button>
-    </div>
-               
+    </div>               
 </form>
 
 @endsection
