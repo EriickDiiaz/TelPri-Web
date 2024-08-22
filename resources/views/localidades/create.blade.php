883b3ae0bb7d2134d41deb2a5a88cb3e77fdc441
Erick Desktop <erick.diaz.1.2@gmail.com>
2024-08-22 18:50:42 -0400

    Se agregan DataTables, Iconos y estilos a sección Localidades

--- resources/views/localidades/create.blade.php
+++ resources/views/localidades/create.blade.php
@@ -6,6 +6,7 @@
 <!-- Mensajes y Notificaciones -->
 @if ($errors->any())
     <div class="alert alert-warning alert-dismissible fade show" role="alert">
+        <i class="fa-solid fa-triangle-exclamation"></i>
         <strong>¡Uy!</strong> Revisa los siguientes errores antes de continuar.
         <ul>
             @foreach($errors->all() as $error)
@@ -18,30 +19,30 @@
 
 <!-- Titulo de la Sección -->
 <div class="d-flex">
-    <i class="bi bi-building" style="font-size:150%;"></i>
-    <h2 class="align-middle">Crear Localidad.</h2>
+    <h2><i class="fa-solid fa-plus m-2"></i>Crear Localidad.</h2>
 </div>
 
 <!--Contenido de la Sección -->
 <form action="{{ url('localidades') }}" method="post">
     @csrf
 
-    <div>
+    <div class="mb-2">
         <label for="nombre" class="col-sm-2 col-form-label">Nombre de localidad:</label>
         <div class="col-sm-5">
             <input type="text" class="form-control" name="nombre" id="nombre" value="{{ old('nombre') }}" required>
         </div>
     </div>
 
-    <div class="mt-3">
+    <div class="mt-3 d-flex justify-content-between col-5">
         <a href="{{ url('localidades') }}" class="btn btn-outline-danger btn-sm">
-            <i class="bi bi-backspace"></i>
-            <span>Regresar</span>
+            <span>
+                <i class="fa-solid fa-delete-left m-2"></i>Regresar
+            </span>
         </a>
-        |
         <button type="submit" class="btn btn-outline-success btn-sm">
-            <i class="bi bi-building-add"></i>
-            <span>Agregar</span>
+            <span>
+                <i class="fa-solid fa-plus m-2"></i>Agregar Localidad
+            </span>
         </button>
     </div>       
 </form>
--- resources/views/localidades/index.blade.php
+++ resources/views/localidades/index.blade.php
@@ -6,7 +6,7 @@
 <!-- Mensajes y Notificaciones -->
 @if(Session::has('mensaje'))
     <div class="alert alert-success alert-dismissible" role="alert">
-        <i class="bi bi-check2-circle"></i>
+        <i class="fa-solid fa-circle-check"></i>
         {{ Session::get('mensaje') }}
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
@@ -14,30 +14,33 @@
 
 <!-- Titulo de la Sección -->
 <div class="d-flex">
-    <i class="bi bi-building align-middle" style="font-size:150%;"></i>
-    <h2 class="align-middle">Administrador de Localidades.</h2>
+    <h2 class="align-middle"><i class="fa-solid fa-location-dot m-2"></i>Administrador de Localidades.</h2>
 </div>
 
 <!-- Botón Agregar -->
 <a href="{{ url('localidades/create') }}" class="btn btn-outline-success btn-sm me-2">
-    <i class="bi bi-building-add"></i>
-    <span class="align-middle">Agregar Localidad</span>
+    <span>
+        <i class="fa-solid fa-plus m-2"></i>Agregar Localidad
+    </span>
+</a>
+<a href="{{ url('pisos/create') }}" class="btn btn-outline-success btn-sm me-2">
+    <span>
+        <i class="fa-solid fa-plus m-2"></i>Agregar Piso
+    </span>
 </a>
 
-<!-- Barra de Búsqueda -->       
-
 <!-- Resumen de Localidades -->
-<div class="d-flex justify-content-between mx-2 py-2 col-8">
+<div class="d-flex py-2 col-8">
     <div class="align-items-center">
         <button class="btn btn-outline-primary btn-sm" disabled>
-            Total:
+            Total Localidades:
             <span class="badge text-bg-primary rounded-pill mx-2">{{ $totalLocalidad }}</span>
         </button>
     </div>
 </div>
 
 <!-- Contenido de Sección -->
-<table class="table table-striped">
+<table class="table table-striped" id="datatableLocalidad">
     <thead>
         <tr>
             <th>ID</th>
@@ -46,72 +49,36 @@
         </tr>
     </thead>
     <tbody>
-        @if(count($localidades)<=0)
-            <tr>
-                <td colspan="7">                      
-                    <div class="alert alert-warning d-flex align-items-center p-2" role="alert">
-                        <i class="bi bi-exclamation-diamond align-middle"></i>
-                        <div class="p-2">
-                            ¡Uy! No hay nada que mostrar.
-                        </div>
-                    </div>
-                </td>
-            </tr>
-        @else
         @foreach ($localidades as $localidad)
         <tr>
             <td>{{ $localidad->id }}</td>
             <td>{{ $localidad->nombre }}</td>
             <td>
-                <a href="{{ url('localidades/'.$localidad->id.'/edit')}}" class="btn btn-outline-primary btn-sm">
-                    <i class="bi bi-pencil-square"></i>
+                <!--Boton Pisos-->
+                <a href="#" class="btn btn-outline-light btn-sm" data-bs-toggle="modal" data-bs-target="">
+                    <i class="fa-solid fa-elevator"></i>
                 </a>
-                |
-
+                <!--Boton Editar-->
+                <a href="{{ url('localidades/'.$localidad->id.'/edit')}}" class="btn btn-outline-primary btn-sm">
+                    <i class="fa-solid fa-pen-to-square"></i>
+                </a>
+                <!--Boton Eliminar-->
                 <form action="{{ url('localidades/'.$localidad->id)}}" id="form-eliminar-{{ $localidad->id }}" action="{{ route('localidades.destroy', $localidad->id) }}" class="d-inline" method="post">
                     @method("DELETE")
                     @csrf
                     <button type="submit" class="btn btn-outline-danger btn-sm">
-                        <i class="bi bi-building-dash"></i>
+                        <i class="fa-solid fa-xmark"></i>
                     </button>
                 </form>
-
-                
             </td>
         </tr>
         @endforeach
-        @endif
     </tbody>
 </table>
 
-<script>
-    document.querySelectorAll('form[id^="form-eliminar-"]').forEach(form => {
-        form.addEventListener('submit', function(event) {
-            event.preventDefault();
-            const formId = this.id;
-
-            Swal.fire({
-                title: '¿Estás seguro de que quieres eliminar esta localidad?',
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Sí, eliminar',
-                cancelButtonText: 'Cancelar',
-                background: '#333',  // Fondo oscuro
-                color: '#fff',       // Texto blanco
-                customClass: {
-                    popup: 'swal2-dark',
-                    title: 'swal2-title',
-                    confirmButton: 'swal2-confirm',
-                    cancelButton: 'swal2-cancel'
-                }
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    document.getElementById(formId).submit();
-                }
-            });
-        });
-    });
-</script>
 @endsection
+
+@push('scripts')
+    @include('partials.datatableLocalidad')
+    @include('partials.sweetalert')
+@endpush
