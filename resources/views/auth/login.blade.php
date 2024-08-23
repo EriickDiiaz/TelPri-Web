7267dab13b1d273b21ee97003d8b92a5f4330229
Erick Desktop <erick.diaz.1.2@gmail.com>
2024-08-22 21:41:09 -0400

    Se modifica navbar de Login. Se elimina layout PDF

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,21 +11,24 @@
             <form method="POST" action="{{ route('login') }}">
                 @csrf
 
+                <div class="mb-2">
+                    <i class="fa-regular fa-user"></i>
+                    <label for="email" class="col-md-4 col-form-label">{{ __('Usuario:') }}</label>
+                    <div class="col-md-6">
+                        
+                        <input type="text" class="form-control" name="extension" id="extension" value="{{ old('extension') }}" required>
+                    </div>
+                </div>
                 <div class="row mb-3"> 
-                    <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Usuario:') }}</label>
+                    
 
                     <div class="col-md-6">
-                        <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                        @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
+                        
                     </div>
                 </div>
 
-                <div class="row mb-3">
+                <div class="mb-3">
+                    <i class="fa-solid fa-lock"></i>
                     <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Contraseña:') }}</label>
 
                     <div class="col-md-6">
