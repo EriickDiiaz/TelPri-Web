5b87f85c2d3a9d1fd4c51b8c10740149f9e81a8d
Erick Desktop <erick.diaz.1.2@gmail.com>
2024-08-24 12:35:25 -0400

    Se modificó layout del login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,75 +2,69 @@
 
 @section('title','TelPri-Web | Inicio de Sesión')
 @section('contenido')
-<div class="container my-3">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="row mb-3 justify-content-center">
-                <img src="{{ asset('imagenes/Logo_TelPriWeb_Wh.png') }}" alt="Logo TelPri" style="width: 40%;" class="py-3 ">
-            </div>
-            <form method="POST" action="{{ route('login') }}">
-                @csrf
-
-                <div class="mb-2">
-                    <i class="fa-regular fa-user"></i>
-                    <label for="email" class="col-md-4 col-form-label">{{ __('Usuario:') }}</label>
-                    <div class="col-md-6">
-                        
-                        <input type="text" class="form-control" name="extension" id="extension" value="{{ old('extension') }}" required>
-                    </div>
-                </div>
-                <div class="row mb-3"> 
-                    
-
-                    <div class="col-md-6">
-                        
-                    </div>
-                </div>
-
-                <div class="mb-3">
-                    <i class="fa-solid fa-lock"></i>
-                    <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Contraseña:') }}</label>
-
-                    <div class="col-md-6">
-                        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                        @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                    </div>
-                </div>
-
-                <div class="row mb-3">
-                    <div class="col-md-6 offset-md-4">
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                            <label class="form-check-label" for="remember">
-                                {{ __('Recuérdame') }}
-                            </label>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="row mb-0">
-                    <div class="col-md-8 offset-md-4">
-                        <button type="submit" class="btn btn-outline-primary">
-                            {{ __('Iniciar sesión') }}
-                        </button>
-
-                        @if (Route::has('password.request'))
-                            <a class="btn btn-link" href="{{ route('password.request') }}">
-                                {{ __('¿Olvidaste tu contraseña?') }}
-                            </a>
-                        @endif
-                    </div>
-                </div>
-            </form>
-                
-            
+<div class="container my-3 d-flex align-items-center justify-content-center" >
+    <div class="col-md-6">
+        <div class="row mb-4 justify-content-center">
+            <img src="{{ asset('imagenes/Logo_TelPriWeb_Wh.png') }}" alt="Logo TelPri" style="width: 40%;" class="py-3">
         </div>
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+
+            <div class="mb-3">
+                <label for="email" class="col-form-label">{{ __('Usuario:') }}</label>
+                <div class="input-group">
+                    <div class="input-group-text">
+                        <i class="fa-solid fa-user"></i>
+                    </div>
+                    <input id="email" type="text" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                    
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label for="password" class="col-form-label">{{ __('Contraseña:') }}</label>
+                <div class="input-group">
+                    <div class="input-group-text">
+                        <i class="fa-solid fa-lock-open"></i>
+                    </div>
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="mb-3 form-check">
+                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                <label class="form-check-label" for="remember">
+                    {{ __('Recuérdame') }}
+                </label>
+            </div>
+
+            <div class="row mb-0 justify-content-center">
+                <div class="col-md-8 text-center">
+                    <button type="submit" class="btn btn-outline-primary">
+                        {{ __('Iniciar sesión') }}
+                    </button>
+            
+                    @if (Route::has('password.request'))
+                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                            {{ __('¿Olvidaste tu contraseña?') }}
+                        </a>
+                    @endif
+                </div>
+            </div>
+            
+        </form>
     </div>
 </div>
 @endsection
+
--- resources/views/layout/auth.blade.php
+++ resources/views/layout/auth.blade.php
@@ -6,81 +6,96 @@
     <title>@yield('title')</title>
     
     <!-- Enlace al CSS de Bootstrap -->
-    <link href="{{ asset('css/bootstrap-5.3.3/css/bootstrap.min.css') }}" rel="stylesheet">
-    <!-- Iconos de Bootstrap -->
-    <link href="{{ asset('css/bootstrap-5.3.3/icons/font/bootstrap-icons.css') }}" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Iconos de Font Awesome -->
+    <script src="https://kit.fontawesome.com/708e2917d6.js" crossorigin="anonymous"></script>
 
     <style>
         /* Estilos personalizados */
-        .admin-navbar {
-            background-color: #0098DA;
+        html, body {
+            height: 100%;
         }
-               
+        .admin-navbar {
+            background: linear-gradient(to right, #7F24EE, #0098DA, #0E15F9);
+        }
+        body {
+            padding-top: 4%; /* Altura del navbar */
+            display: flex;
+            flex-direction: column;
+        }
+        /* Ajuste de espaciado para el contenido principal */
+        .footer{          
+            padding: 10px 0;    
+            position: relative;
+            bottom: 0;
+            text-align: center;
+        }               
     </style>
 </head>
 <body>
 
 <!-- Navbar para administración de usuario -->
-
-<nav class="navbar navbar-expand-md admin-navbar shadow-sm">
-    <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Lado Izquierdo de NavBar -->
-            <ul class="navbar-nav me-auto">
-                
-            </ul>
-
-            <!-- Lado Derecho de NavBar -->
-            <ul class="navbar-nav ms-auto">
-                <!-- Authentication Links -->
-                @guest
-                    @if (Route::has('login'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">
-                                <i class="bi bi-box-arrow-in-right"></i> {{ __('Ingresar') }}
-                            </a>
-                        </li>
-                    @endif
-
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">
-                                <i class="bi bi-check-square"></i> {{ __('Registrar') }}
-                            </a>
-                        </li>
-                    @endif
-                @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            <i class="bi bi-person"></i> {{ Auth::user()->name }}
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">
-                                <i class="bi bi-box-arrow-right"></i> {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
+<nav class="navbar navbar-expand-lg admin-navbar fixed-top">
+    <div class="container-fluid">
+        <div class="col-4">
+            <i class="fa-regular fa-calendar"></i>
+            <span id="fechaHora">{{ $fechaHoraActual }}</span>
         </div>
+        
+        <div class="col-4">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item dropdown">
+                    <ul class="navbar-nav ms-auto">
+                    <!-- Authentication Links -->
+                    @guest
+                        @if (Route::has('login'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">
+                                    <i class="fa-solid fa-right-to-bracket"></i> {{ __('Ingresar') }}
+                                </a>
+                            </li>
+                        @endif
+
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">
+                                    <i class="fa-solid fa-check-to-slot"></i> {{ __('Registrar') }}
+                                </a>
+                            </li>
+                        @endif
+                    @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                <i class="fa-solid fa-user"></i> {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ url('usuarios/'.Auth::user()->id.'/edit')}}">
+                                    <i class="fa-solid fa-gear"></i> {{ __('Modificar Usuario') }}
+                                </a>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                    <i class="fa-solid fa-right-to-bracket"></i> {{ __('Cerrar Sesión') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                        @endguest
+                    </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>   
     </div>
 </nav>
 
 <!-- Contenido principal -->
-<main class="px-2" style="min-height: 580px;">
+<main style="">
     @yield('contenido')
 </main>
 
