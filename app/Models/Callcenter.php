3db403937cff0ecf5974b4ad5a94c9995aeaa128
Erick Diaz <erick.diaz.1.2@gmail.com>
2024-10-23 10:25:38 -0400

    Callcenters: Se modifica actualiza modelo agregando el atributo fillable a los campos de la tabla

--- app/Models/Callcenter.php
+++ app/Models/Callcenter.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Callcenter extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'extension',
+        'nombres',
+        'usuario',
+        'contrasena',
+        'servicio',
+        'acceso',
+    ];
 }
